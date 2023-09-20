0f7175cd5afcd77e6435d043b76fc37759521696
phreis <philippr01@yahoo.de>
2023-09-20 16:13:01 +0200

    Create page structure and the correspoinding css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import arrowWhite from './AndyHooke/62c465b0c5c60d512ddd0779_right-arrow-white.svg';
+import clogo1 from './AndyHooke/62c47e2a4720fe33f10bd88f_parkvestors-logo.png';
+import clogo5 from './AndyHooke/62c47e2a9cc59548dedc7e9c_Landing Fox Logo.png';
+import clogo2 from './AndyHooke/62c47e2abd976733e34cb120_Enpointe logo.png';
+import clogo3 from './AndyHooke/62c47e2aee2b66dbf7cd2b47_Switch4Good logo.png';
+import clogo4 from './AndyHooke/62c47e3191478f0f08a8ae7e_Gaia Guru Logo.png';
 import conceptHero from './AndyHooke/62c6bb066902bfc92596d8dd_concept-hero (1).jpg';
 import logo from './AndyHooke/62c7f86e12d1acb4787312ab_Andy-Hooke-pfp-crop-small.jpg';
 import arrow from './AndyHooke/62c80e4cfe2a39725202fc60_right-arrow-purple.svg';
@@ -52,7 +57,6 @@ export default function App() {
             Agency-quality Webflow websites with the personal touch of a
             freelancer.
           </p>
-
           <div className={styles.buttonContainer}>
             <a className={styles.baseButton} href="/#">
               <div className={styles.buttonArrowContainer}>
@@ -60,7 +64,6 @@ export default function App() {
                 <img src={arrowWhite} alt="arrowWhite" />
               </div>
             </a>
-
             <a
               className={`${styles.baseButton} ${styles.buttonBright}`}
               href="/#"
@@ -68,13 +71,31 @@ export default function App() {
               Learn more
             </a>
           </div>
-          <div className={styles.heroImageContainer}>
-            <img src={conceptHero} alt="conceptHero" />
+          <div className={`${styles.heroImageContainer} ${styles.transform}`}>
+            <img
+              className={styles.animate}
+              src={conceptHero}
+              alt="conceptHero"
+            />
           </div>
         </div>
       </header>
-      <section className={styles.darkSection}>
-        <div className={styles.pagepadding}>section1</div>
+      <section
+        className={`${styles.pagepadding} ${styles.firstSection} ${styles.darkSection}`}
+      >
+        <p className={styles.textSizeLargeWhite}>
+          8+ YEARS OF DESIGN EXPERIENCE
+        </p>
+        <div className={styles.logoContainer}>
+          <img src={clogo1} alt="clogo1" />
+          <img src={clogo2} alt="clogo2" />
+          <img src={clogo3} alt="clogo3" />
+          <img src={clogo4} alt="clogo4" />
+          <img src={clogo5} alt="clogo5" />
+        </div>
+        <br />
+        <br />
+        <br />
       </section>
       <section className={styles.lightSection}>
         <div className={styles.pagepadding}>section2</div>
